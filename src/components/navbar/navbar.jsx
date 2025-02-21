3c8cfcdcd87228ac573b6e36210b65bbe39f7f8b
Gabriel Neves <129914892+Gabrielneves05@users.noreply.github.com>
2025-02-21 13:27:14 -0300

    creating new pages / implementing routes

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,6 +2,7 @@ import styles from "./navbar.module.css";
 import { LuShoppingCart, LuUserRound, LuMenu } from "react-icons/lu";
 import { Drawer } from "@mui/material";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -13,16 +14,22 @@ export default function Navbar() {
   return (
     <nav className={styles.navbarContainer}>
       <div className={styles.navbarItems}>
-        <img 
-          className={styles.logo} 
-          src="/logo.png" 
-          alt="Logo" 
-        />
+        <Link to={"/"} className={styles.logoContainer}>
+          <img 
+            className={styles.logo} 
+            src="/logo.png" 
+            alt="Logo" 
+          />
+        </Link>
         <div className={styles.navbarLinksContainer}>
-          <a href="#" className={styles.navbarLink}>Início</a>
-          <a href="#" className={styles.navbarLink}>Pratos</a>
-          <LuShoppingCart className={styles.navbarLink} />
-          <LuUserRound className={styles.navbarLink} />
+          <Link to={"/"} className={styles.navbarLink}>Início</Link>
+          <Link to={"/plates"} className={styles.navbarLink}>Pratos</Link>
+          <Link to={"/cart"} className={styles.navbarLink}>
+            <LuShoppingCart  />
+          </Link>
+          <Link to={"/profile"} className={styles.navbarLink}>
+            <LuUserRound />
+          </Link>
         </div>
       </div>
 
